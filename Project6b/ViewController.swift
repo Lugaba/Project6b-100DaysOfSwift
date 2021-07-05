874230a3609fe58366c49376f1ed135200ed30b1
Luca Hummel <luca.hummel12@gmail.com>
2021-07-05 08:57:22 -0300

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Luca Hummel on 05/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
